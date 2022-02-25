2122e2ee55b6948b958a92e115e3ba1fed75793f
ivan <imalashenko22@gmail.com>
2022-02-25 21:50:34 +0300

    added endless loop of game

--- Rock-Paper-Scissors/task/game.py
+++ Rock-Paper-Scissors/task/game.py
@@ -3,15 +3,22 @@ options = ["scissors", "rock", "paper"]
 beat_dict = {"scissors": "rock", "paper": "scissors", "rock": "paper"}
 
 user_choice = input()
-comp_choice = __import__("random").choice(options)
-res = ""
-message = (f"There is a draw ({comp_choice})",
-           f"Sorry, but the computer chose {comp_choice}",
-           f"Well done. The computer chose {comp_choice} and failed")
-if user_choice == comp_choice:
-    res = 0
-elif beat_dict[user_choice] == comp_choice:
-    res = 1
-else:
-    res = 2
-print(message[res])
+while True:
+    if user_choice == "!exit":
+        break
+    if user_choice not in options:
+        print("Invalid input")
+        continue
+    comp_choice = __import__("random").choice(options)
+    res = ""
+    message = (f"There is a draw ({comp_choice})",
+               f"Sorry, but the computer chose {comp_choice}",
+               f"Well done. The computer chose {comp_choice} and failed")
+    if user_choice == comp_choice:
+        res = 0
+    elif beat_dict[user_choice] == comp_choice:
+        res = 1
+    else:
+        res = 2
+    print(message[res])
+print("Bye!")
